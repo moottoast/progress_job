2c8accd1f0086e72a09a2ba45f8913f87e10083e
Jeff Juszczak <jeff@moottoast.com>
2017-05-02 15:13:38 -0400

    Render nothing if delayed job is nil

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -3,7 +3,9 @@ module ProgressJob
 
     def show
       @delayed_job = Delayed::Job.find_by_id(params[:job_id])
-      unless @delayed_job.nil?
+      if @delayed_job.nil?
+        render nothing: true
+      else
         percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
         render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
       end
