e55c207636f75f88be38f5fd7bda4d5ca4952650
Jeff Juszczak <jeff@moottoast.com>
2017-05-02 15:06:57 -0400

    Don't throw an exception if we can't find a delayed job

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -2,7 +2,7 @@ module ProgressJob
   class ProgressController < ActionController::Base
 
     def show
-      @delayed_job = Delayed::Job.find(params[:job_id])
+      @delayed_job = Delayed::Job.find_by_id(params[:job_id])
       unless @delayed_job.nil?
         percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
         render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
