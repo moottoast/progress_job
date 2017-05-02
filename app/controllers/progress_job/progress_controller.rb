eca38e06d8f1c91dc0945350bf19dca5496a6755
Jeff Juszczak <jeff@moottoast.com>
2017-05-02 09:24:52 -0400

    Don't render anything on delayed_job being nil

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -3,8 +3,10 @@ module ProgressJob
 
     def show
       @delayed_job = Delayed::Job.find(params[:job_id])
-      percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
-      render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
+      unless @delayed_job.nil
+        percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
+        render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
+      end
     end
 
   end
